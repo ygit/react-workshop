5806a7a185b940bc4cfc7bee5797f78c274a3b0e
Yogesh <yogesh.singh.2810@gmail.com>
2018-04-21 12:59:55 +0530

    added span tab event action handler

--- geekabyte-workshop/src/App.js
+++ geekabyte-workshop/src/App.js
@@ -3,20 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  onTabChange(event){
+    console.log(event)
+    console.log("onTabChange invoked")
+  }
+
   render() {
     return (
       <div className="App">
 
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to ReactJS at Of10 by Geekabyte 🚀</h1>
+          <h1 className="App-title">Welcome to ReactJS at Of10 by Geekabyte</h1>
         </header>
 
         <div>
-          <span className="tab" onClick={e => console.log(e)}>Now Showing</span>
-          <span className="tab" onClick={e => console.log(e)}>Top Rated</span>
+          <span className="tab" onClick={this.onTabChange}>Now Showing</span>
+          <span className="tab" onClick={this.onTabChange}>Top Rated</span>
         </div>
-        
+
       </div>
     );
   }
